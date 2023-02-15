dc4da4ed152fd36e807a8ade9450fe3b4c058d87
Victor DE LEUSSE <vde-leus@e1r11p22.clusters.42paris.fr>
2023-02-15 12:57:39 +0100

    Rendu

--- Pipex/Pipex/free.d
+++
@@ -1,3 +0,0 @@
-Pipex/free.o: Pipex/free.c Pipex/pipex.h Pipex/../Libft/libft.h \
-  Pipex/../Libft/libft_old.h Pipex/../Libft/get_next_line.h \
-  Pipex/../Libft/ft_printf.h
--- Pipex/Pipex/msg.d
+++
@@ -1,3 +0,0 @@
-Pipex/msg.o: Pipex/msg.c Pipex/pipex.h Pipex/../Libft/libft.h \
-  Pipex/../Libft/libft_old.h Pipex/../Libft/get_next_line.h \
-  Pipex/../Libft/ft_printf.h
